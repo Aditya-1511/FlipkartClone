31966f6c47c4cdb82d9a84588ded743a65afdb30
Aditya <aditya2.mobcoder@gmail.com>
2022-05-23 12:26:33 +0530

    MySQL connection established

--- Instagram_mysql_mvc/config/dbConfig.js
+++ Instagram_mysql_mvc/config/dbConfig.js
@@ -1,19 +1,19 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-async function dbConnection(){
-const sequelize = new Sequelize('instagram_mysql', 'Aditya', 'Aditya@123', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+async function dbConnection() {
+  const sequelize = new Sequelize("instagram_mysql", "Aditya", "Aditya@123", {
+    host: "localhost",
+    dialect: "mysql",
+  });
 
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+  try {
+    await sequelize.authenticate();
+    console.log("MySQL Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
 module.exports = {
-  dbConnection
-}
+  dbConnection,
+};
