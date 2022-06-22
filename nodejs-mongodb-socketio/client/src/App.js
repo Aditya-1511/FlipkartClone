66ab1ce87da3e2760c05d7f410c942e2efa89036
Aditya <aditya2.mobcoder@gmail.com>
2022-06-22 10:00:26 +0530

    Chat app using socket.io

---
+++ nodejs-mongodb-socketio/client/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import io from 'socket.io-client';
+import {useEffect} from "react";
+
+const socket = io.connect("http://localhost:3005");
+
+
+function App() {
+  const sendMessage = () =>{
+    socket.emit("send_message",{message:"Hello"});
+  };
+
+  //listening broadcast message
+  useEffect(()=>{
+    socket.on("receive_message", (data)=>{
+      alert(data.message);
+    })
+  },[socket]);
+
+  return (
+    <div className="App">
+      <input placeholder="Message..." /> <br/>
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
+  );
+}
+
+export default App;
