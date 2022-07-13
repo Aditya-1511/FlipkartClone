eaf408ca4c48d101811b20decca13384ec95d677
dev-aditya-pandey <creation.1511@gmail.com>
2022-07-13 09:57:26 +0530

    Formatted code

--- Search service/server.js
+++ Search service/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,15 +9,18 @@ app.use(
   })
 );
 
-
 // searching on query
-app.get('/search/:index/:type', async (req, res) => {
-  const { phraseSearch } = require('./SearchEngine');
-  const data = await phraseSearch(req.params.index, req.params.type, req.query.q);
-//   res.send(data);
-// console.log(data);
-// console.log(req.params.index)
-res.json(req.params.index, req.params.type, req.query.q)
+app.get("/search/:index/:type", async (req, res) => {
+  const { phraseSearch } = require("./SearchEngine");
+  const data = await phraseSearch(
+    req.params.index,
+    req.params.type,
+    req.query.q
+  );
+  //   res.send(data);
+  // console.log(data);
+  // console.log(req.params.index)
+  res.json(req.params.index, req.params.type, req.query.q);
 });
 
-app.listen(3000, () => console.log('server running at 3000'));
+app.listen(3000, () => console.log("server running at 3000"));
